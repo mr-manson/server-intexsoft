cb094db920dd185e8ccd69598976bdeaacfbd38a
mr-manson <nikola777jr@gmail.com>
2023-05-25 23:15:28 +0300

    first get 'hello world'

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
 app.listen(port, () => {
     console.log(`Server starts on port ${port}`)
 });
