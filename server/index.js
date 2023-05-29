4ccbbbf411ca87a0fb39514113d5018361116119
mr-manson <nikola777jr@gmail.com>
2023-05-29 23:19:08 +0300

    update index

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const routs = require("../routs/routes");
-const {sequelize, connect} = require('../db/db.js');
+const {connect} = require('../db/db.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-/*app.use(function (req, res, next){
+app.use(function (req, res, next){
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST");
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
     res.setHeader("Access-Control-Allow-Credentials", true);
-})*/
+    next();
+})
 app.use(express.json());
 app.use("/api", routs);
 
